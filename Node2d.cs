c655a5caa622ce4e51c96cf3129dd86573a3b5aa
cowburrs <brycetabangcura@gmail.com>
2025-03-02 10:57:52 +1100

    Added Collision Shape to hexagon
    I don't even know why tghis exists

--- Node2d.cs
+++ Node2d.cs
@@ -7,12 +7,17 @@ public partial class Node2d : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Line2D line = GetNode<Line2D>("Line2D");
 		Position = GetViewportRect().Size / 2;
+		Line2D line = GetNode<Line2D>("Line2D");
 		for(int i = 0; i < 7; i++){
 			line.AddPoint(new Vector2(0, Size).Rotated(Mathf.Pi / (6 / 2) * i));
-			GD.Print(new Vector2(0, Size).Rotated(Mathf.Pi / (6 / 2) * i));
 		}
+		CollisionPolygon2D collision = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
+		Vector2[] colArray = new Vector2[6];	
+		for(int i = 0; i < 6; i++){
+			colArray[i] = new Vector2(0, Size).Rotated(Mathf.Pi / (6 / 2) * i);
+		}
+		collision.Polygon = colArray;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

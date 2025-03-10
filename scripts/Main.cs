f93d9c234c94c9719e894cfef04bb8c97dd4e03c
Niko-172 <niko.russo@outlook.com>
2025-03-10 15:22:36 +1100

    Restructured into a main

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,14 +1,12 @@
 using Godot;
 using System;
-using System.Diagnostics;
 
-public partial class Node2d : Node2D
+public partial class Main : Node2D
 {
-	[Export] public int Size {get; set;} = 200;
-
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Position = GetViewportRect().Size / 2;
+		GD.Print("Hellow");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

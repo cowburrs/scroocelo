4bd7d4de7e4b56ab777759b8229421478c41016b
cowburrs <kazousatuma@proton.me>
2025-09-15 18:41:51 +1000

    windows pc time

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -3,17 +3,17 @@ using System;
 
 public partial class Player : RigidBody3D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            GD.Print("Hello");
-        }
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			GD.Print("Hello");
+		}
+	}
 }
